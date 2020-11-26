8ec54c4255d0d2e7418d6092b4d88c3f3bdd56cb
michael <michael@ignition-innovations.com>
2020-11-25 23:10:49 -0500

    added idp section

--- assets/js/bg-animation.js
+++ assets/js/bg-animation.js
@@ -2,20 +2,32 @@
 const intro = document.querySelector('.intro');
 const video = intro.querySelector('video');
 const text = intro.querySelector('h1');
+const text2 = intro.querySelector('.intro_text_2');
 //Fulani Section
 const fulani = document.querySelector('.fulani');
-const fulani_text = fulani.querySelector('h1');
-fulani.setAttribute('style','background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("assets/img/fulani.jpg");');
+fulani.setAttribute('style','background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("assets/img/fulani.jpg");');
 fulani.style.backgroundSize = 'cover';
 fulani.style.backgroundRepeat = 'no-repeat';
+const fulani_title = fulani.querySelector('h1');
+const fulani_text_1 = fulani.querySelector('.fulani_text_1');
+const fulani_text_2 = fulani.querySelector('.fulani_text_2');
+//IDP SECTION
+const idp = document.querySelector('.idp');
+idp.setAttribute('style','background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("https://cdn.statically.io/img/nigeria.ignition633.org/wp-content/uploads/2020/07/IDP-camp2-2.jpeg") 50% 50%;');
+idp.style.backgroundSize = 'cover';
+idp.style.backgroundRepeat = 'no-repeat';
+const idp_title = idp.querySelector('h1');
+const idp_text_1 = idp.querySelector('.idp_text_1');
+const idp_text_2 = idp.querySelector('.idp_text_2');
+const idp_text_3 = idp.querySelector('.idp_text_3');
 //END SECTION
-//const section = document.querySelector('section');
-//const end = section.querySelector('h1');
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
 
 //Scroll Magic
 const controller = new ScrollMagic.Controller();
 
-//Scenes
+//Intro Scenes
 let scene = new ScrollMagic.Scene({
   duration: 9000,
   triggerElement: intro,
@@ -31,12 +43,17 @@ let scene = new ScrollMagic.Scene({
 .addTo(controller);
 
 //Text Animation
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+const textAnim = TweenMax.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
+//Text Animation 3
+const textAnim3 = TweenMax.fromTo(text2, 2, { opacity: 1 }, { opacity: 0 });
+//Text Animation 2
+const textAnim2 = TweenMax.fromTo(text2, 2, { opacity: 0 }, { opacity: 1 });
 
 let scene2 = new ScrollMagic.Scene({
-  duration: 3000,
+  //duration: 2000,
   triggerElement: intro,
-  triggerHook: 0
+  triggerHook: 0,
+  offset: 150
 })
 .addIndicators({
   name: "Intro Text Trigger",
@@ -48,23 +65,184 @@ let scene2 = new ScrollMagic.Scene({
 .addTo(controller);
 
 
-//Text Animation 2
-const textAnim2 = TweenMax.fromTo('.fulani_text', 3, { opacity: 0 }, { opacity: 1 });
-
 let scene3 = new ScrollMagic.Scene({
-  duration: 3000,
-  triggerElement: '.fulani',
+  //duration: 2000,
+  triggerElement: intro,
   triggerHook: 0,
+  offset: 2000
 })
 .addIndicators({
-  name: "Fulani Text Trigger",
+  name: "Intro Text2 Trigger",
   colorTrigger: "blue",
   colorStart: "blue",
   colorEnd: "blue"
 })
 .setTween(textAnim2)
+.addTo(controller);
+
+
+let scene4 = new ScrollMagic.Scene({
+  //duration: 2000,
+  triggerElement: intro,
+  triggerHook: 0,
+  offset: 4000
+})
+.addIndicators({
+  name: "Intro Text3 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim3)
+.addTo(controller);
+
+
+
+//FULANI SCENES
+
+//Text Animation 4
+const textAnim4 = TweenMax.fromTo(fulani_title, 2, { opacity: 0 }, { opacity: 1 });
+//Text Animation 5
+const textAnim5 = TweenMax.fromTo(fulani_text_1, 2, { opacity: 0 }, { opacity: 1 });
+//Text Animation 6
+const textAnim6 = TweenMax.fromTo(fulani_text_2, 2, { opacity: 0 }, { opacity: 1 });
+
+let scene5 = new ScrollMagic.Scene({
+  duration: 2000,
+  triggerElement: '.fulani',
+  triggerHook: 0,
+})
+.addIndicators({
+  name: "Fulani Title Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
 .setPin(fulani)
 .addTo(controller);
+
+let scene6 = new ScrollMagic.Scene({
+  triggerElement: '.fulani',
+  triggerHook: 0,
+})
+.addIndicators({
+  name: "Fulani Title Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim4)
+.addTo(controller);
+
+let scene7 = new ScrollMagic.Scene({
+  triggerElement: '.fulani',
+  triggerHook: 0,
+  offset: 500
+})
+.addIndicators({
+  name: "Fulani Text1 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim5)
+.addTo(controller);
+
+let scene8 = new ScrollMagic.Scene({
+  triggerElement: '.fulani',
+  triggerHook: 0,
+  offset: 1000
+})
+.addIndicators({
+  name: "Fulani Text1 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim6)
+.addTo(controller);
+
+
+//IDP Scenes
+
+//Text Animation 7
+const textAnim7 = TweenMax.fromTo(idp_title, 2, { opacity: 0 }, { opacity: 1 });
+//Text Animation 8
+const textAnim8 = TweenMax.fromTo(idp_text_1, 2, { opacity: 0 }, { opacity: 1 });
+//Text Animation 9
+const textAnim9 = TweenMax.fromTo(idp_text_2, 2, { opacity: 0 }, { opacity: 1 });
+//Text Animation 10
+const textAnim10 = TweenMax.fromTo(idp_text_3, 2, { opacity: 0 }, { opacity: 1 });
+
+let scene9 = new ScrollMagic.Scene({
+  duration: 2500,
+  triggerElement: idp,
+  triggerHook: 0,
+})
+.addIndicators({
+  name: "IDP Title Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setPin(idp)
+.addTo(controller);
+
+let scene10 = new ScrollMagic.Scene({
+  triggerElement: idp,
+  triggerHook: 0,
+})
+.addIndicators({
+  name: "IDP Title Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim7)
+.addTo(controller);
+
+let scene11 = new ScrollMagic.Scene({
+  triggerElement: idp,
+  triggerHook: 0,
+  offset: 500
+})
+.addIndicators({
+  name: "IDP Text1 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim8)
+.addTo(controller);
+
+let scene12 = new ScrollMagic.Scene({
+  triggerElement: idp,
+  triggerHook: 0,
+  offset: 1000
+})
+.addIndicators({
+  name: "IDP Text2 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim9)
+.addTo(controller);
+
+let scene13 = new ScrollMagic.Scene({
+  triggerElement: idp,
+  triggerHook: 0,
+  offset: 1500
+})
+.addIndicators({
+  name: "IDP Text3 Trigger",
+  colorTrigger: "blue",
+  colorStart: "blue",
+  colorEnd: "blue"
+})
+.setTween(textAnim10)
+.addTo(controller);
+
 
 
 //Video Animation
@@ -79,6 +257,6 @@ scene.on('update', e => {
 
 setInterval(() => {
   delay += (scrollpos - delay) * accelAmount;
-  console.log(scrollpos, delay);
+  //console.log(scrollpos, delay);
   video.currentTime = delay;
 }, 166);//change the refresh rate depending on the frame rate of the video.
--- index.php
+++ index.php
@@ -9,28 +9,65 @@ $cache_buster = uniqid();
   <body>
     
       <div class="intro">
-        <h1>
-          The Issues
-        </h1>
         <video>
           <source src="assets/video/nigeria-slums.mp4" type="video/mp4">
           Your browser does not support this video type...
         </video>
+        <h1>
+          These are the Issues...
+        </h1>
+        <h1 class="intro_text_2" style="opacity:0;">
+          Facing Nigeria Today
+        </h1>
       </div>
           
       <div class="fulani">
-        <h1 class="fulani_text">
-          Fulani Herdsmen
+        <h1 class="fulani_title">
+          Fulani Herdsmen Oppression
         </h1>
-        <!--<img src="assets/img/fulani.jpg" />-->
+        <h2 class="fulani_text_1">
+          The Fulani are an Islamic tribe throughout West Africa with a significant population in Nigeria. 
+          They are predominantly nomadic, and make their living by herding and trading cattle. There are many 
+          radicalized groups of Fulani who have taken up the Islamic Jihad movement and have targeted Christian 
+          villages to seize lands needed for their cattle to graze, as well as to achieve their extreme religious 
+          beliefs by exterminating Christians.
+        </h2>
+        <h2 class="fulani_text_2">
+          These Fulani Herdsman attacks are DEVESTATING. They pillage and burn down the villages. 
+          Many people are ruthlessly killed in their wake of destruction. The villages are purposely 
+          left uninhabitable and those who escape seek never to return.
+        </h2>
       </div>
     
-      <!--<section>
-        <h1>
-          Demo Animation
+      <div class="idp">
+        <h1 class="idp_title">
+          Internally Displaced People [IDP] Camps
         </h1>
-        <img src="assets/img/fulani.jpg" />
-      </section>-->
+        <h2 class="idp_text_1">
+          The people came into the camps carrying only what they could 
+          grab as they fled for their lives. There is currently no organized effort in place to 
+          assist this forgotten group. When our team visited for the first time to assess the crises 
+          and pray directly with the distraught refugees, two requests immediately emerged:
+        </h2>
+        <br>
+        <h2 class="idp_text_2">
+          <span>The ability to bathe.</span> The women in the camps whispered privately to the women on our team 
+          that they needed water to bathe. Most of the people in the camps have no ability to wash 
+          themselves or attend to their most basic hygiene needs, and go days without bathing in 
+          temperatures normally above 100F. Many of the camps do not have good access to clean water 
+          and certainly not soap or other cleaning products.
+        </h2>
+        <br>
+        <h2 class="idp_text_3">
+          <span>Food.</span> Food is extremely limited. There is constant urgent need for even basic food provisions.
+        </h2>
+      </div>
+    
+      <section>
+        <h1>
+          End Section
+        </h1>
+      </section>
         
   </body>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/ScrollMagic.min.js" integrity="sha512-8E3KZoPoZCD+1dgfqhPbejQBnQfBXe8FuwL4z/c8sTrgeDMFEnoyTlH3obB4/fV+6Sg0a0XF+L/6xS4Xx1fUEg==" crossorigin="anonymous"></script>
